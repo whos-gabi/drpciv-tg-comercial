0d4bb6bcef16e7955892c7587da1bec4d75f45d5
whos-gabi <56652297+whos-gabi@users.noreply.github.com>
2023-05-11 21:05:12 +0300

    quick fix

--- appointmentChecker.js
+++ appointmentChecker.js
@@ -23,54 +23,52 @@ function appointmentChecker() {
         //   "Judet: ",
         //   judete[user.data.judetId - 1].nume
         // );
-        if (user.data.archived) {
-          return;
-        }
-        //validate user free trial
-        if (subsLayer.validateUserTrial(user.data)) {
-          await subsLayer
-            .getDateDRPCIV(user.data.judetId)
-            .then(async (date) => {
-              // console.log(
-              //   "fetched date: " + date + " last date: " + user.data.lastDate
-              // );
-              if (date !== user.data.lastDate) {
-                // console.log("new date found: " + date);
-                await subsLayer
-                  .updateUserLastDate(user.data.chat.id, date)
-                  .then(async () => {
-                    await subsLayer.sendMessage(
-                      user.data.chat.id,
-                      `${date} este noua data disponibila la DRPCIV`
-                    );
-                  });
-              }
-            });
-        } else {
-          try {
-            await subsLayer.sendMessage(
-              process.env.ADMIN_CHAT_ID,
-              `User ${user.data.chat.id} expired trial\n\n${JSON.stringify(
-                user.data,
-                null,
-                2
-              )}`
-            );
-            await subsLayer.sendMessage(
-              user.data.chat.id,
-              `Abonamentul dvs. a expirat.😥\nPentru a continua sa primiti notificari, va rugam sa achitati abonamentul`
-            );
-            // await subsLayer.archiveUser(user.data.chat.id, true);
-            await subsLayer.updateUserTrial(user.data.chat.id, false);
-          } catch (err) {
-            console.log(err);
+        if (!user.data.archived) {
+          //validate user free trial
+          if (subsLayer.validateUserTrial(user.data)) {
+            await subsLayer
+              .getDateDRPCIV(user.data.judetId)
+              .then(async (date) => {
+                // console.log(
+                //   "fetched date: " + date + " last date: " + user.data.lastDate
+                // );
+                if (date !== user.data.lastDate) {
+                  // console.log("new date found: " + date);
+                  await subsLayer
+                    .updateUserLastDate(user.data.chat.id, date)
+                    .then(async () => {
+                      await subsLayer.sendMessage(
+                        user.data.chat.id,
+                        `${date} este noua data disponibila la DRPCIV`
+                      );
+                    });
+                }
+              });
+          } else {
+            try {
+              await subsLayer.sendMessage(
+                process.env.ADMIN_CHAT_ID,
+                `User ${user.data.chat.id} expired trial\n\n${JSON.stringify(
+                  user.data,
+                  null,
+                  2
+                )}`
+              );
+              await subsLayer.sendMessage(
+                user.data.chat.id,
+                `Abonamentul dvs. a expirat.😥\nPentru a continua sa primiti notificari, va rugam sa achitati abonamentul`
+              );
+              await subsLayer.archiveUser(user.data.chat.id, true);
+              await subsLayer.updateUserTrial(user.data.chat.id, false);
+            } catch (err) {
+              console.log(err);
+            }
           }
         }
+
       });
     });
   }, 60 * 1000 * 1); // 1 min
 }
-
-
 
 appointmentChecker();
